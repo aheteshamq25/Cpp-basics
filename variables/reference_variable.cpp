6e9678c6214d56d57f534d3407dc77c31df93f33
ahetesham <aheteq45@gmail.com>
2026-06-03 11:15:17 +0530

    typecasting and reference variables

--- variables/reference_variable.cpp
+++ variables/reference_variable.cpp
@@ -5,15 +5,22 @@ using namespace std;
 int c = 34;
 
 int main(){
-    int a, b, c;
-    cout<<"Enter the number a: ";
-    cin>>a;
-    cout<<"Enter the number b: ";
-    cin>>b;
+    // int a, b, c;
+    // cout<<"Enter the number a: ";
+    // cin>>a;
+    // cout<<"Enter the number b: ";
+    // cin>>b;
 
-    c = a + b ;
-    cout<<"the sum is c = "<<c<<endl;
-    cout<<"The globle value of c ="<<::c;  // ---> [::] it will give us value of global c.
+    // c = a + b ;
+    // cout<<"the sum is c = "<<c<<endl;
+    // cout<<"The globle value of c ="<<::c;  // ---> [::] it will give us value of global c.
+
+    // ************ Reference variable *******************
+    float x = 445;
+    cout<<x<<endl;
+
+    float & y = x;
+    cout<<y<<endl;
 
     return 0;
 
---
+++ variables/typecasting.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+// ******Typecasting *********
+
+int main(){
+    int a = 45;
+    float b = 46.454;
+
+    cout<<"the value of a is "<<(float)a<<endl;
+    cout<<"the value of b is "<<(int)b<<endl;
+
+    int c = int(b);
+    
+
+
+    return 0;
+}
