f1ffa411d4e38c32e59a1a77f78187cebf3192e7
ahetesham <aheteq45@gmail.com>
2026-08-13 18:49:26 +0530

    student details using file input output

--- file Input Output/file_input_output.cpp
+++ file Input Output/file_input_output.cpp
@@ -5,25 +5,25 @@
 using namespace std;
 
 int main(){
-    ofstream out;
-    out.open("this.txt");
-    out<<"Hello this is me Ahetesham"<<endl;
-    out<<"I am a computer science student"<<endl;
-    out<<"I am studing in 2nd year."<<endl;
+    // ofstream out;
+    // out.open("this.txt");
+    // out<<"Hello this is me Ahetesham"<<endl;
+    // out<<"I am a computer science student"<<endl;
+    // out<<"I am studing in 2nd year."<<endl;
 
-    out.close();
+    // out.close();
 
-    // ifstream in;
-    // string st , st2;
-    // in.open("this.txt");
+    ifstream in;
+    string st , st2;
+    in.open("this.txt");
 
-    // while (in.eof()==0)
-    // {
-    //     getline(in , st);
-    //     cout<<st<<endl;
-    // }
+    while (in.eof()==0)
+    {
+        getline(in , st);
+        cout<<st<<endl;
+    }
 
-    // in.close();
+    in.close();
     
     
 }
